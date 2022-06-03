34e8b8f930e588a63557c4c97bae491ece002e8e
RATIU5 <jcmemmott20@outlook.com>
2022-06-02 18:26:53 -0600

    add(navbar.tsx): added html structure for navbar

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -3,15 +3,18 @@ import * as React from "react";
 export default function Navbar() {
 	return (
 		<nav>
+			<div>
+				<img src="/images/logo.png" alt="RATIU5 logo" width={50} />
+			</div>
 			<ul>
 				<li>
-					<a href="#">Link 1</a>
+					<a href="/">profile</a>
 				</li>
 				<li>
-					<a href="#">Link 2</a>
+					<a href="/blog">blog</a>
 				</li>
 				<li>
-					<a href="#">Link 3</a>
+					<a href="/contact">contact</a>
 				</li>
 			</ul>
 		</nav>
