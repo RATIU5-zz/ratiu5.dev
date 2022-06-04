52ba9ebc1d45658c021cf20b979a88895ab4812d
RATIU5 <jcmemmott20@outlook.com>
2022-06-04 13:26:38 -0600

    edit(.tsx): added basic template for a post

---
+++ app/routes/blog/$postId.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+export default function PostPage() {
+	return (
+		<article>
+			<h1>post title</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+				voluptates aliquam iusto deleniti, labore eius ad aspernatur in aut
+				doloremque atque quaerat corrupti dolor quo laborum nobis! Nostrum,
+				quibusdam temporibus? Lorem ipsum dolor sit amet consectetur adipisicing
+				elit. Voluptates incidunt veniam provident, illo maxime magni,
+				repellendus dicta facilis suscipit qui architecto, sint non corrupti at
+				debitis nobis amet ab quidem? Lorem ipsum dolor sit amet, consectetur
+				adipisicing elit. Amet temporibus minima provident aspernatur autem quia
+				repellendus velit a explicabo deserunt, dicta voluptatum exercitationem
+				illo porro. Inventore libero iusto praesentium vel. Lorem ipsum dolor
+				sit amet consectetur adipisicing elit. In exercitationem quia, rem
+				voluptates error porro, earum quod doloremque fugit numquam consectetur
+				quas rerum, ipsum maxime magni odio corporis animi accusantium.
+			</p>
+		</article>
+	);
+}
